a24bc72f617a19d4ea381da8102c21d4df793c7e
hitesh09saini <hitesh9694saini@gmail.com>
2023-09-24 17:34:14 +0530

    first

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    const data = {
+        message: 'Hello, world!',
+        name: 'Hitesh',
+        class: 25,
+    };
+    res.json(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
